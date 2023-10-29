dfb21e992645a20cfc70f8e7068341e58e985099
Sanil <sanilsarathe76@gmail.com>
2023-10-29 23:41:29 +0530

    Added like button Ui with count

--- Roomie/ViewController/Gallary/TableViewXIB/GallaryTableViewCell.swift
+++ Roomie/ViewController/Gallary/TableViewXIB/GallaryTableViewCell.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+struct GallaryModel {
+    var likesCount: Int
+    var likes: [Int]
+    var isliked: Bool
+}
+
+enum likeUnlike {
+    case like
+    case unlike
+}
+
 class GallaryTableViewCell: UITableViewCell {
 
     static let identifier = "GallaryTableViewCell"
@@ -15,6 +26,9 @@ class GallaryTableViewCell: UITableViewCell {
     @IBOutlet weak var gallaryLabel: UILabel!
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var heartLikeUnlikeButton: UIButton!
+    
+    var isLiked: likeUnlike = .unlike
+    var count = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,9 +41,26 @@ class GallaryTableViewCell: UITableViewCell {
     }
     
     @IBAction func onClickLikeUnlike(_ sender: UIButton) {
-        
+        switch isLiked {
+        case .unlike:
+            isLiked = .like
+            setLikeUnlikeButtonUI()
+        case .like:
+            isLiked = .unlike
+            setLikeUnlikeButtonUI()
+        }
     }
     
+    func setLikeUnlikeButtonUI() {
+        switch isLiked {
+        case .like:
+            heartLikeUnlikeButton.setImage(UIImage(named: "HeartLikeIcon"), for: .normal)
+            countLabel.text = "\(count + 1)"
+        case .unlike:
+            heartLikeUnlikeButton.setImage(UIImage(named: "HeartUnLikeIcon"), for: .normal)
+            countLabel.text = "\(count)"
+        }
+    }
     
     func setData(data: GallaryImages) {
         gallaryImageView?.image = UIImage(named: data.images)
